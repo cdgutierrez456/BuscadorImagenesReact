9970a6a3ba00d4b01413eb1e5fb06f0e587b04ef
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-28 22:24:29 -0500

    Agregando componente de paginacion. Terminando detalles de funcionalidad

--- src/App.js
+++ src/App.js
@@ -8,23 +8,70 @@ class App extends Component {
 
   state = {
     termino: '',
-    imagenes: []
+    imagenes: [],
+    pagina: ''
+  }
+
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'start');
+  }
+
+  paginaAnterior = () => {
+    // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    // Definir si la pagina es 1, si lo es... cancelar accion
+    if(pagina === 1) return null;
+
+    // Resta uno a la pagina inicial
+    pagina--;
+
+    // Agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    // console.log(pagina);
+  }
+
+  paginaSiguiente = () => {
+    // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    // Sumar uno a la pagina inicial
+    pagina++;
+
+    // Agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    // console.log(pagina);
   }
 
   consultarApi = () => {
     const termino = this.state.termino;
-    const url = `https://pixabay.com/api/?key=19963715-4f70db65d59908dc7a1a2b103&q=${termino}&per_page=30`;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=19963715-4f70db65d59908dc7a1a2b103&q=${termino}&per_page=30&page=${pagina}`;
     // console.log(url);
 
     fetch(url)
       .then(respuesta => respuesta.json())
-      .then(resultado => this.setState({imagenes: resultado.hits}))
+      .then(resultado => this.setState({ imagenes: resultado.hits }))
 
   }
 
   datosBusqueda = (termino) => {
     this.setState({
-      termino
+      termino: termino,
+      pagina: 1
     }, () => {
       this.consultarApi();
     })
@@ -39,9 +86,13 @@ class App extends Component {
             datosBusqueda={this.datosBusqueda}
           />
         </div>
-        <Resultado
-          imagenes = {this.state.imagenes}
-        />
+        <div className="row justify-content-center">
+          <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+          />
+        </div>
       </div>
     );
   }
