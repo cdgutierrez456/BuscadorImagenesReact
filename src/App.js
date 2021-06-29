68de58b26610537cf8010f0949e394318915f29c
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-28 19:54:49 -0500

    Capturando termino de busqueda. Obteniendo array con resultados. Creado componente Resultado.js para inyectar resultados

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import Buscador from './components/Buscador';
+import Resultado from './components/Resultado';
 
 
 
 class App extends Component {
 
   state = {
-    termino: 'Café'
+    termino: '',
+    imagenes: []
   }
 
-  cosultarApi = () => {
-    const url = `https://pixabay.com/api/?key=19963715-4f70db65d59908dc7a1a2b103&q=${this.state.termino}`;
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=19963715-4f70db65d59908dc7a1a2b103&q=${termino}&per_page=30`;
+    // console.log(url);
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({imagenes: resultado.hits}))
+
   }
 
   datosBusqueda = (termino) => {
@@ -30,7 +39,7 @@ class App extends Component {
             datosBusqueda={this.datosBusqueda}
           />
         </div>
-        {this.state.termino}
+        <Resultado/>
       </div>
     );
   }
